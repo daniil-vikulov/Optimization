d49ce0fdb45ae78ec90287ac9004c0f3670be29a
kuchina-anastasia11 <112166202+kuchina-anastasia11@users.noreply.github.com>
2024-02-08 17:14:09 +0300

    Update Tests.h

--- tests/Tests.h
+++ tests/Tests.h
@@ -4,4 +4,13 @@
 
 namespace adaai::tests {
     void checkGmpLib();
+
+    //@brief test exponent function in float mode
+    void testFloat();
+
+    ///@brief test exponent function in double mode
+    void testDouble();
+
+    ///@brief test exponent function in long double mode
+    void testLongDouble();
 }
