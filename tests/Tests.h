701b65a4c6b90474bd7ed8d8b55f4ddea8648b66
kuchina-anastasia11 <112166202+kuchina-anastasia11@users.noreply.github.com>
2024-02-25 20:40:10 +0300

    added chebyshev-tests

--- tests/Tests.h
+++ tests/Tests.h
@@ -1,15 +1,24 @@
 #ifndef TESTS_H
 #define TESTS_H
 
+#include "Logger.h"
+
 namespace adaai::tests {
-    ///@brief checks whether GSL library has been linked correctly
-    void testGslLib();
+    ///@brief checks whether GMP library has been linked correctly
+    void checkGmpLib();
 
-    ///@brief testing of Taylor-based exponent calculation for float, double and long double
-    void testTaylor();
+    ///@brief testing of Chebyshev-based exponent calculation for float, double and long double
+    void testChebyshev();
 
-    ///@brief testing of Pade-based exponent calculation for float, double and long double
-    void testPade();
+    //@brief internal Chebyshev-based exponent testing function in float mode
+    void testFloatChebyshev();
+
+    ///@brief internal Chebyshev-based exponent testing function in double mode
+    void testDoubleChebyshev();
+
+    ///@brief internal Chebyshev-based exponent testing function in long double mode
+    void testLongDoubleChebyshev();
+
 }
 
 #endif
