8862c921a6f629ac1d0ae223cea3b34647df39f8
kuchina-anastasia11 <kuchina-anastasia11@gmail.com>
2024-03-12 19:42:34 +0300

    Added cos, sin, exp

--- AAD22.cpp
+++ AAD22.cpp
@@ -112,3 +112,52 @@ AAD22 operator /(AAD22 const& value, double const& right) {
 }
 //TODO: add /= 
 
+
+//sin differentiator 
+AAD22 AAD22::derivative_sin() const {
+    AAD22 res = *this;
+    res.m_val = sin(res.m_val);
+    res.m_d1[0] = res.m_d1[0] * cos(this->m_val);
+    res.m_d1[1] = res.m_d1[1] * cos(this->m_val);
+    res.m_d2[0] = res.m_d2[0] * cos(this->m_val) - this->m_d1[0] * this->m_d1[0] * sin(this->m_val);
+    res.m_d2[1] = res.m_d2[1] * cos(this->m_val) - this->m_d1[1] * this->m_d1[1] * sin(this->m_val);
+    res.m_d2[2] = res.m_d2[2] * cos(this->m_val) - this->m_d1[0] * this->m_d1[1] * sin(this->m_val);
+    return res; 
+}
+
+AAD22 sin(AAD22 const& value) {
+    return value.derivative_sin();
+}
+
+//cos differentiator
+AAD22 AAD22::derivative_cos() const {
+    AAD22 res = *this;
+    res.m_val = sin(res.m_val);
+    res.m_d1[0] = -1 * res.m_d1[0] * sin(this->m_val);
+    res.m_d1[1] = -1 * res.m_d1[1] * sin(this->m_val);
+    res.m_d2[0] = -1 * res.m_d2[0] * sin(this->m_val) - this->m_d1[0] * this->m_d1[0] * cos(this->m_val);
+    res.m_d2[1] = -1 * res.m_d2[1] * sin(this->m_val) - this->m_d1[1] * this->m_d1[1] * cos(this->m_val);
+    res.m_d2[2] = -1 * res.m_d2[2] * sin(this->m_val) - this->m_d1[0] * this->m_d1[1] * cos(this->m_val);
+    return res; 
+}
+
+AAD22 cos(AAD22 const& value) {
+    return value.derivative_cos();
+}
+
+//exp differentiator
+AAD22 AAD22::derivative_exp() const {
+    AAD22 res = *this;
+    res.m_val = exp(res.m_val);
+    res.m_d1[0] = res.m_val * res.m_d1[0];
+    res.m_d1[1] = res.m_val * res.m_d2[0];
+    res.m_d2[0] = res.m_val * (this->m_d1[0] * this->m_d1[0] + res.m_d2[0]);
+    res.m_d2[1] = res.m_val * (this->m_d1[1] * this->m_d1[1] + res.m_d2[1]);
+    res.m_d2[2] = res.m_val * (this->m_d1[0] * this->m_d1[1] + res.m_d2[2]);
+    return res; 
+}
+
+AAD22 exp(AAD22 const& value) {
+    return value.derivative_exp();
+}
+
