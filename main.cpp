88cd99e6ca3f7c8dabf7952fb64f66bf8134680c
daniil-vikulov <daniel.vikulov@yandex.ru>
2024-03-18 19:44:50 +0300

    Initial commit: 1) Pulled project structure from previous labs

---
+++ main.cpp
@@ -0,0 +1,5 @@
+#include "tests/Tests.h"
+
+int main () {
+
+}
---
+++ tests/Tests.h
@@ -0,0 +1,11 @@
+#ifndef TESTS_H
+#define TESTS_H
+
+#include "utils/Logger.h"
+#include "include/core/FFTExp.h"
+
+namespace adaai::tests {
+    //TODO
+}
+
+#endif
