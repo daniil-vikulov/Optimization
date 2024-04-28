a2c4c0d0265d53bc3055debcea4112caba1ccc26
daniil-vikulov <daniel.vikulov@yandex.ru>
2024-04-28 20:48:25 +0300

    Adjusted EHIntegrator 1) added documentation documentary comments 2) added TODO-s 3) started implementing methods

--- include/maths/EHIntegrator.h
+++ include/maths/EHIntegrator.h
@@ -9,13 +9,27 @@ namespace adaai::solution {
     typedef void (*Equation)(double t_0, const double *y, const double y_prime, double *f, void *params);
 
     class EHIntegrator {
+    private:
+        ///@brief parameters of the equation
+        void *Parameters{};
+        ///@brief vectors dimension
+        double N;
+        ///@brief coefficients in the
+        double *B{};
+        ///@brief number of interpolation nodes
+        double K;
+
+        //TODO implement divided difference
+
     public:
-        EHIntegrator(Equation equation, int n, void *params);
+        EHIntegrator(Equation equation, int dimension, int node_number, void *parameters = nullptr);
 
         ~EHIntegrator();
 
         ///@brief calculates y(t_0 + h) and y'(t_0 + h), where y(t_0) = y_0 and y'(t_0) = y_prime_0
         void integrate(double *y_0, double *y_prime_0, double t_0, double h);
 
+    private:
+        void step(double *y_0, double *y_prime_0, double t_0, double h);
     };
 }
--- src/EHIntegrator.cpp
+++ src/EHIntegrator.cpp
@@ -3,14 +3,25 @@
 
 using namespace adaai::solution;
 
-EHIntegrator::EHIntegrator(Equation equation, int n, void *params) {
-
+EHIntegrator::EHIntegrator(Equation equation, int dimension, int node_number, void *parameters) {
+    K = node_number;
+    N = dimension;
+    B = new double[dimension];
+    Parameters = parameters;
 }
 
 EHIntegrator::~EHIntegrator() {
-
+    delete[] B;
 }
 
 void EHIntegrator::integrate(double *y_0, double *y_prime_0, double t_0, double h) {
-    //TODO WTF
+    int steps = 15; // TODO should be dynamically calculated (convergence-based)
+
+    for (int i = 0; i < steps; ++i) {
+        step(y_0, y_prime_0, t_0, h);
+    }
+}
+
+void EHIntegrator::step(double *y_0, double *y_prime_0, double t_0, double h) {
+
 }
