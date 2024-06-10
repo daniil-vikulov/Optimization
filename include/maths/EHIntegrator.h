facf759258904d4b5c14121244fb14703d203863
daniil-vikulov <daniel.vikulov@yandex.ru>
2024-06-11 00:18:31 +0300

    Integrator's logic ready. fillB is required debugging is required

--- include/maths/EHIntegrator.h
+++ include/maths/EHIntegrator.h
@@ -6,12 +6,12 @@
 
 namespace adaai::solution {
     ///Pointer to the function, which specifies how f(t_0, y(t_0), y'(t_0)) will be calculated in the equation: y'' = f(t, y, y')
-    ///@param t_0
+    ///@param t0
     ///@param y - vector y(t_0)
-    ///@param y_prime - vector y'(t_0)
+    ///@param yPrime - vector y'(t_0)
     ///@param params - additional parameters used for calculation
     ///@return f - will contain vector f(t_0, y(t_0), y'(t_0))
-    typedef std::vector<double> (*Equation)(double t_0, std::vector<double> &y, const std::vector<double> &y_prime,
+    typedef std::vector<double> (*Equation)(double t0, std::vector<double> &y, const std::vector<double> &yPrime,
                                             void *params);
 
     class EHIntegrator {
@@ -25,13 +25,19 @@ namespace adaai::solution {
         constexpr int K = 6;
 
         ///@brief dimension of vectors to operate with. Say, y, y', y''. Correspondingly, f: R^{2*n+1} - > R^n
-        double _dimension;
+        int _dimension;
 
-        ///@brief double
-        std::vector<double> _b;
+        ///@brief [nodeIndex][dimension]
+        std::vector<std::vector<double> > _yInNodes;
 
-        ///@brief double[n][k], containing devided difference
-        std::vector<std::vector<double> > _dd;
+        ///@brief [nodeIndex][dimension]
+        std::vector<std::vector<double> > _yPrimeInNodes;
+
+        ///@brief [nodeIndex][dimension]
+        std::vector<std::vector<double> > _b;
+
+        ///@brief double[nodeIndex][order][dimension], containing devided difference
+        std::vector<std::vector<std::vector<double> > > _dd;
 
     public:
         EHIntegrator(Equation equation, int dimension, void *parameters = nullptr);
@@ -40,17 +46,17 @@ namespace adaai::solution {
 
         ///@brief calculates y(t_0 + h) and y'(t_0 + h) where y(t_0) = y_0 and y'(t_0) = y_prime_0
         ///@details fills vectors with desired data
-        void integrate(std::vector<double> &y_0, std::vector<double> &y_prime_0, double t_0, double h);
+        void integrate(std::vector<double> &y0, std::vector<double> &yPrime0, double t0, double h);
 
     private:
         ///@brief calculates approximation for y(t_0 + h), y'(t_0 + h), y''(t_0 + h)
         ///@details Fills vectors with desired data. Evoke multiple times with the same paramters to increase accuracy
-        void step(std::vector<double> &y_0, std::vector<double> &y_prime_0, double t_0, double h);
+        void step(double t0, double h);
 
         ///@brief fills _b array with coefficients, calculated with _dd
-        void fillB(std::vector<double> &y, std::vector<double> &y_prime);
+        void fillB(double t0);
 
         ///@brief fills _dd array with divided difference
-        void fillDD(std::vector<double> &y, std::vector<double> &y_prime);
+        void fillDD(double t0);
     };
 }
--- src/EHIntegrator.cpp
+++ src/EHIntegrator.cpp
@@ -1,5 +1,6 @@
 #include "maths/EHIntegrator.h"
 
+#include <cmath>
 
 using namespace adaai::solution;
 
@@ -8,29 +9,65 @@ EHIntegrator::EHIntegrator(const Equation equation, const int dimension, void *p
     _dimension = dimension;
     _parameters = parameters;
     _b.resize(K);
-    _dd.resize(K, std::vector<double>(K));
+    _dd.resize(K, std::vector(K, std::vector<double>(_dimension)));
+    _yInNodes.resize(K, std::vector<double>(_dimension));
+    _yPrimeInNodes.resize(K, std::vector<double>(_dimension));
 }
 
-
-void EHIntegrator::integrate(std::vector<double> &y_0, std::vector<double> &y_prime_0, const double t_0,
+void EHIntegrator::integrate(std::vector<double> &y0, std::vector<double> &yPrime0, const double t0,
                              const double h) {
-    constexpr int interations = 15;
+    constexpr int interations = 15; //TODO make dynamic
+
+    std::copy(y0.begin(), y0.end(), _yInNodes[0].begin());
+    std::copy(yPrime0.begin(), yPrime0.end(), _yPrimeInNodes[0].begin());
 
     for (int i = 0; i < interations; ++i) {
-        step(y_0, y_prime_0, t_0, h);
+        step(t0, h);
+    }
+
+    std::copy(_yInNodes[K - 1].begin(), _yInNodes[K - 1].end(), y0.begin());
+    std::copy(_yPrimeInNodes[K - 1].begin(), _yPrimeInNodes[K - 1].end(), yPrime0.begin());
+}
+
+void EHIntegrator::step(const double t0, const double h) {
+    fillB(t0);
+
+    for (int i = 1; i < K; ++i) {
+        for (int j = 0; j < _dimension; ++j) {
+            _yPrimeInNodes[i][j] = _yPrimeInNodes[0][j];
+            for (int k = 0; k < K; ++k) {
+                _yPrimeInNodes[i][j] += _b[k][i] * std::pow(h * i, k + 1) / (k + 1);
+            }
+        }
+    }
+
+    for (int i = 1; i < K; ++i) {
+        for (int j = 0; j < _dimension; ++j) {
+            _yInNodes[i][j] = -_yInNodes[0][j] + _yPrimeInNodes[i][j];
+            for (int k = 0; k < K; ++k) {
+                _yInNodes[i][j] += _b[k][i] * std::pow(h * i, k + 2) / (k + 1) / (k + 2);
+            }
+        }
     }
 }
 
-void EHIntegrator::step(std::vector<double> &y_0, std::vector<double> &y_prime_0, double t_0, double h) {
-    fillB(y_0, y_prime_0);
+void EHIntegrator::fillB(const double t0) {
+    fillDD(t0);
 
-
-}
-
-void EHIntegrator::fillB(std::vector<double> &y, std::vector<double> &y_prime) {
     //TODO calculate _b
 }
 
-void EHIntegrator::fillDD(std::vector<double> &y, std::vector<double> &y_prime) {
-    //TODO calculate _dd
+void EHIntegrator::fillDD(const double t0) {
+    for (int j = 0; j < K; ++j) {
+        _dd[j][0] = _equation(t0, _yInNodes[j], _yPrimeInNodes[j], nullptr);
+    }
+
+    for (int order = 1; order < K; ++order) {
+        for (int nodeIndex = 0; nodeIndex < K; ++nodeIndex) {
+            for (int dim = 0; dim < _dimension; ++dim) {
+                _dd[nodeIndex][order][dim] = (_dd[nodeIndex + 1][order - 1][dim] - _dd[nodeIndex][order - 1][dim]) /
+                                             order;
+            }
+        }
+    }
 }
