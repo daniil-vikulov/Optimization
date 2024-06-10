d028f16a12756d43299bdbca48e732fd24873d0f
daniil-vikulov <daniel.vikulov@yandex.ru>
2024-06-10 23:25:44 +0300

    Initial integrator logic Specified headers

--- include/maths/EHIntegrator.h
+++ include/maths/EHIntegrator.h
@@ -1,35 +1,56 @@
 #pragma once
+#include <vector>
+
+
+/// NOTE: C-like interface for full GSL-compatibility in terms of arguments and appearance:)
 
 namespace adaai::solution {
     ///Pointer to the function, which specifies how f(t_0, y(t_0), y'(t_0)) will be calculated in the equation: y'' = f(t, y, y')
+    ///@param t_0
     ///@param y - vector y(t_0)
     ///@param y_prime - vector y'(t_0)
-    ///@param f - will contain vector f(t_0, y(t_0), y'(t_0))
     ///@param params - additional parameters used for calculation
-    typedef void (*Equation)(double t_0, const double *y, const double y_prime, double *f, void *params);
+    ///@return f - will contain vector f(t_0, y(t_0), y'(t_0))
+    typedef std::vector<double> (*Equation)(double t_0, std::vector<double> &y, const std::vector<double> &y_prime,
+                                            void *params);
 
     class EHIntegrator {
-    private:
-        ///@brief parameters of the equation
-        void *Parameters{};
-        ///@brief vectors dimension
-        double N;
-        ///@brief coefficients in the
-        double *B{};
-        ///@brief number of interpolation nodes
-        double K;
+        ///@brief RHS of the ODE: (t, y(t), y'(t))
+        Equation _equation;
 
-        //TODO implement divided difference
+        ///@brief parameters of the equation
+        void *_parameters{};
+
+        ///@brief number of interpolation nodes (t_0, t_1 .. t_{k-1}, t_k = t_0 + h)
+        constexpr int K = 6;
+
+        ///@brief dimension of vectors to operate with. Say, y, y', y''. Correspondingly, f: R^{2*n+1} - > R^n
+        double _dimension;
+
+        ///@brief double
+        std::vector<double> _b;
+
+        ///@brief double[n][k], containing devided difference
+        std::vector<std::vector<double> > _dd;
 
     public:
-        EHIntegrator(Equation equation, int dimension, int node_number, void *parameters = nullptr);
+        EHIntegrator(Equation equation, int dimension, void *parameters = nullptr);
 
-        ~EHIntegrator();
+        ~EHIntegrator() = default;
 
-        ///@brief calculates y(t_0 + h) and y'(t_0 + h), where y(t_0) = y_0 and y'(t_0) = y_prime_0
-        void integrate(double *y_0, double *y_prime_0, double t_0, double h);
+        ///@brief calculates y(t_0 + h) and y'(t_0 + h) where y(t_0) = y_0 and y'(t_0) = y_prime_0
+        ///@details fills vectors with desired data
+        void integrate(std::vector<double> &y_0, std::vector<double> &y_prime_0, double t_0, double h);
 
     private:
-        void step(double *y_0, double *y_prime_0, double t_0, double h);
+        ///@brief calculates approximation for y(t_0 + h), y'(t_0 + h), y''(t_0 + h)
+        ///@details Fills vectors with desired data. Evoke multiple times with the same paramters to increase accuracy
+        void step(std::vector<double> &y_0, std::vector<double> &y_prime_0, double t_0, double h);
+
+        ///@brief fills _b array with coefficients, calculated with _dd
+        void fillB(std::vector<double> &y, std::vector<double> &y_prime);
+
+        ///@brief fills _dd array with divided difference
+        void fillDD(std::vector<double> &y, std::vector<double> &y_prime);
     };
 }
--- src/EHIntegrator.cpp
+++ src/EHIntegrator.cpp
@@ -3,25 +3,34 @@
 
 using namespace adaai::solution;
 
-EHIntegrator::EHIntegrator(Equation equation, int dimension, int node_number, void *parameters) {
-    K = node_number;
-    N = dimension;
-    B = new double[dimension];
-    Parameters = parameters;
+EHIntegrator::EHIntegrator(const Equation equation, const int dimension, void *parameters) {
+    _equation = equation;
+    _dimension = dimension;
+    _parameters = parameters;
+    _b.resize(K);
+    _dd.resize(K, std::vector<double>(K));
 }
 
-EHIntegrator::~EHIntegrator() {
-    delete[] B;
-}
 
-void EHIntegrator::integrate(double *y_0, double *y_prime_0, double t_0, double h) {
-    int steps = 15; // TODO should be dynamically calculated (convergence-based)
+void EHIntegrator::integrate(std::vector<double> &y_0, std::vector<double> &y_prime_0, const double t_0,
+                             const double h) {
+    constexpr int interations = 15;
 
-    for (int i = 0; i < steps; ++i) {
+    for (int i = 0; i < interations; ++i) {
         step(y_0, y_prime_0, t_0, h);
     }
 }
 
-void EHIntegrator::step(double *y_0, double *y_prime_0, double t_0, double h) {
+void EHIntegrator::step(std::vector<double> &y_0, std::vector<double> &y_prime_0, double t_0, double h) {
+    fillB(y_0, y_prime_0);
 
+
+}
+
+void EHIntegrator::fillB(std::vector<double> &y, std::vector<double> &y_prime) {
+    //TODO calculate _b
+}
+
+void EHIntegrator::fillDD(std::vector<double> &y, std::vector<double> &y_prime) {
+    //TODO calculate _dd
 }
